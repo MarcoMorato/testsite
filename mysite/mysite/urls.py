73d3bd13a779eba311fe1a2e3ec2d841003ad2a9
MarcoMorato <87669918+MarcoMorato@users.noreply.github.com>
2021-12-02 14:35:14 +0300

    изчение django "[WebForMySelf] Django. Полное руководство (2020)"" финал части

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+import debug_toolbar
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('', include('news.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
+    path('captcha/', include('captcha.urls')),
+]
